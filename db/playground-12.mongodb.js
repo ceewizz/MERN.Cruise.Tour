efd69bd3c21822976d12ea2fc10e747243d48337
ceewizz <ceewizzhuhwin@gmail.com>
2024-03-24 23:45:16 -0700

    added db

---
+++ db/playground-12.mongodb.js
@@ -0,0 +1,13 @@
+/* global use, db */
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+use('cruise-tour');
+
+// Create a new document in the collection.
+db.getCollection('categories').insertOne({
+  _id: ObjectId('660115fe28dce427df4f7279'),
+  name: 'Solo',
+  __v: 0
+});
