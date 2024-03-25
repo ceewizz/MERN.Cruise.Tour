452dd231b6035a24dd206059fb55cd44c5a7fb35
ceewizz <ceewizzhuhwin@gmail.com>
2024-03-25 03:31:59 -0700

    Newsletter and fixed typo

---
+++ server/models/Newsletter.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const newsletterSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+});
+
+const Newsletter = mongoose.model('Newsletter', newsletterSchema);
+
+module.exports = Newsletter;
