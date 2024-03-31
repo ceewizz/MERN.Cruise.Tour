cf3a0ce6aadd11d9799acd634cafedbfd6febc31
Chris Huynh <146052258+ceewizz@users.noreply.github.com>
2024-03-31 04:09:41 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   db.once('open', () => {
     app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      console.log(`Use GraphQL at 100.20.92.101:${PORT}${server.graphqlPath}`);
     })
 
   })
