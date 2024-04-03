c0c77cc2e230818c8f0af0dedc63f01d523e1dd2
ceewizz <ceewizzhuhwin@gmail.com>
2024-04-02 22:50:44 -0700

    fixed

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,7 +1,7 @@
 const db = require('./connection');
 const { User, Product, Category } = require('../models');
 import smallfamImage from ('../client/images/family4.png');
-import bigfamImage from ('../client/images/family7.png'); 
+ 
 db.once('open', async () => {
     await Category.deleteMany();
 
@@ -15,13 +15,13 @@ db.once('open', async () => {
     console.log('categories seeded');
 
     await Product.deleteMany();
-
+   
     const products = await Product.insertMany([
         {
             name: 'Family Package 4 days Baja Mexico',
             category: categories[0]._id,
             description: 'This package is a family package is for a Family of 4 for 4 days cruise starting from  Long beach > Catalina > Ensenada > Back to Long Beach. Prices includes a 2 room 2 person(s) per room, one free Buffet per family of 4 is included. On board activities included: Waterworks, Swimming Pool movie cinema. Additional cost activities: Spa, Clubs, Casino.',
-            image: 'smallfamImage',
+            image: <img src={smallfamImage} style={{ width: "auto"}} alt="fam4"/>,
             price: 1000.00,
             quantity: 20
         },
@@ -29,7 +29,7 @@ db.once('open', async () => {
             name: 'Family Package 7 days Europe',
             category: categories[0]._id,
             description: 'Destination sailing starts from Lisbon > La Coruna > Bilbao > Le Verdon > Paris > London.  Price includes a 2 rooms 2 person(s) per room. 3 free dinner buffet ticket per person for a family of 4,  On board activities included: Waterworks, Swimming Pool movie cinema. Additional cost activities: Spa, Clubs, Casino.',
-            image: 'bigfamImage',
+            image: 'about.png',
             price: 4000.00,
             quantity: 20
 
